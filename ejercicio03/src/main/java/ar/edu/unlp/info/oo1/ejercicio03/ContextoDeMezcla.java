37321f3b9d7a98de023995828cf662c4615227e2
Martin Ezequiel Rodriguez <martinrodrig2001@gmail.com>
2024-07-29 17:06:04 -0300

    creo proyectos del resto de ejercicios

---
+++ ejercicio03/src/main/java/ar/edu/unlp/info/oo1/ejercicio03/ContextoDeMezcla.java
@@ -0,0 +1,22 @@
+package ar.edu.unlp.info.oo1.ejercicio03;
+
+import java.util.ArrayList;
+
+public class ContextoDeMezcla {
+    private EstrategiaDeMezcla estrategia;
+
+
+    public ContextoDeMezcla() {
+        this.estrategia = new EstrategiaDeMezclaIntercalar();
+    }
+
+
+    public ArrayList<String> mezclarColecciones(ArrayList<String> coleccion1, ArrayList<String> coleccion2) {
+        if (coleccion1.size() != coleccion2.size()) {
+            throw new IllegalArgumentException("Las colecciones deben tener la misma longitud");
+        } else if (coleccion1.size() <= 5) {
+            throw new IllegalArgumentException("Las colecciones deben tener longitud mayor a 5");
+        }
+        return estrategia.mezclar(coleccion1, coleccion2);
+    }
+}
---
+++ ejercicio03/src/test/java/ar/edu/unlp/info/oo1/ejercicio03/ejercicio03test.java
@@ -0,0 +1,52 @@
+package ar.edu.unlp.info.oo1.ejercicio03;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+class Ejercicio03Test {
+    
+    private ContextoDeMezcla contexto;
+    private ArrayList<String> lista1;
+    private ArrayList<String> lista2;
+    private ArrayList<String> lista3;
+    private ArrayList<String> lista4;
+
+    @BeforeEach
+    public void setUp() {
+        contexto = new ContextoDeMezcla();
+        lista1 = new ArrayList<>(Arrays.asList("A", "B", "C", "D", "E", "F"));
+        lista2 = new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5", "6"));
+        lista3 = new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5"));
+		lista4 = new ArrayList<>(Arrays.asList("A", "B", "C", "D", "E"));
+
+    }
+
+    @Test
+    public void testMezclarColeccionesMismoTamanoMayorA5() {
+        ArrayList<String> resultadoEsperado = new ArrayList<>(Arrays.asList("A", "1", "B", "2", "C", "3", "D", "4", "E", "5", "F", "6"));
+        assertEquals(resultadoEsperado, contexto.mezclarColecciones(lista1, lista2));
+    }
+    
+	@Test
+	public void testMezclarColeccionesMismoTamanoMenorA6(){
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+			contexto.mezclarColecciones(lista3, lista4);
+		});
+		assertEquals("Las colecciones deben tener longitud mayor a 5", exception.getMessage());
+
+	}
+
+	@Test
+	public void testMezclarColeccionesConTamanoDiferente() {
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+			contexto.mezclarColecciones(lista1, lista3);
+		});
+		assertEquals("Las colecciones deben tener la misma longitud", exception.getMessage());
+	}
+
+}
